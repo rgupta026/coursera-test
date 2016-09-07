15e5d53a14a6a1c77b9e81b4dc4ff16a7d40555a
Radhika <radhikagupta026@gmail.com>
2016-09-07 13:17:27 +0530

    mod4 solution

--- mod4_solution/harder/script.js
+++ mod4_solution/harder/script.js
@@ -9,14 +9,15 @@
 // The program should say "Hello" to any name except names that start with a "J"
 // or "j", otherwise, the program should say "Good Bye". So, the final output
 // on the console should look like this:
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i =0; i < names.length; i++) {
-		var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter == 'j') {
-		byeSpeaker.speak(names[i]);
-  } else {
-		helloSpeaker.speak(names[i]);
-  }
-}
+(function (){
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for (var i =0; i < names.length; i++) {
+			var firstLetter = names[i].charAt(0).toLowerCase();
+	
+	if (firstLetter == 'j') {
+			byeSpeaker.speak(names[i]);
+	} else {
+			helloSpeaker.speak(names[i]);
+	}
+	}
+})();
